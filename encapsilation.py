099dcca1d1d62bc97bebf15aca9da8e82d8e778c
Danielhmiller <danielmillerusmc3043@outlook.com>
2023-04-23 21:32:37 -0500

    Create encapsilation.py

---
+++ encapsilation.py
@@ -0,0 +1,22 @@
+class Protected:
+    def __init__(self):
+        self._protectedVar = 0
+        obj = Protected()
+        obj._protectedVar = 24
+        print(obj._protectedVar)
+
+class Protected:
+
+    def __init__(self):
+        self._privateVar = 111
+
+    def getPrivate(self):
+        print(self._privateVar)
+
+    def setPrivate(self, private):
+        self._privateVar = private
+
+        obj = Protected()
+        obj.getPrivate()
+        obj.setPrivate(1998)
+        obj.getPrivate()
